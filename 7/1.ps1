921119197f7847337508477b74993789c4106f33
JBerggren <jonatan.berggren@gmail.com>
2021-12-17 16:18:59 +0100

    Added data for day 7

---
+++ 7/1.ps1
@@ -0,0 +1,3 @@
+$lines = get-content input.txt
+//$parts = $line.split($separators,[System.StringSplitOptions]::RemoveEmptyEntries) | % {[int]$_}
+Räkna ut median och sen hur mycket varje har till den
